f216386c2f530b9bd1fb97207eae86f932d8f25c
jin lezama <jingoto22@gmail.com>
2020-02-20 16:51:55 -0700

    added new portfolio file

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Jin Lezama Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
   }
