91fb537a9d16a6331763c31d96f4d0b87f7c8e73
jin lezama <jingoto22@gmail.com>
2020-02-21 10:20:38 -0700

    added super function

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -5,10 +5,15 @@ import portfolioItem from './portfolio-item';
 
 
 export default class portfolioContainer extends Component {
+    constructor() {
+        super();
+        
+        console.log("Portfolio container has rendered")
+    }
     render() {
         return (
             <div className='portfolioContainer'>
-                <h2>Portfolio items go here...</h2>
+                <h2>Portfolio items go here updated...</h2>
 
                 <PortfolioItem/>
             </div>
