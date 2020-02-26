2a8f86d82f33bf7f527b916c5a1d84e5b1dc4d82
jin lezama <jingoto22@gmail.com>
2020-02-26 11:22:19 -0700

    added styles scss

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class NavigationComponent extends Component {
   constructor() {
@@ -7,12 +8,26 @@ export default class NavigationComponent extends Component {
 
   render() {
     return (
-      <div>
-        <button>Home</button>
-        <button>About</button>
-        <button>Contact</button>
-        <button>Blog</button>
-        {false ? <button>Add Blog</button> : null}
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
+
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+
+          {false ? <button>Add Blog</button> : null}
+        </div>
+
+        <div className="right-side">JIN LEZAMA</div>
       </div>
     );
   }
