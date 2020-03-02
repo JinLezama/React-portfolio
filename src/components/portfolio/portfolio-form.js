a08af0ed5532b25211c76bd297fb20f0609f530a
jin lezama <jingoto22@gmail.com>
2020-03-02 14:10:46 -0700

    added a text area tag

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -108,7 +108,7 @@ export default class PortfolioForm extends Component {
           </div>
 
           <div>
-            <input
+            <textarea
               type="text"
               name="description"
               placeholder="Description"
