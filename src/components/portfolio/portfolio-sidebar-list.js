b8397c0bfe809a3fc066630115e37e239104c16f
jin lezama <jingoto22@gmail.com>
2020-03-03 15:36:21 -0700

    Building an Edit Click Handler in React

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -10,12 +10,22 @@ const PortfolioSidebarList = props => {
         </div>
         <div className="text-content">
           <div className="title">{portfolioItem.name}</div>
+
+          <div className="actions">
             <a 
-              className="delete-icon" 
+              className="action-icon" 
+              onClick={() => props.handleEditClick(portfolioItem)}
+            >
+              <FontAwesomeIcon icon="edit" />
+            </a> 
+            <a 
+              className="action-icon" 
               onClick={() => props.handleDeleteClick(portfolioItem)}
             >
-            <FontAwesomeIcon icon="trash" />
-          </a>
+              <FontAwesomeIcon icon="trash" />
+            </a> 
+            
+          </div>
         </div>
       </div>
     );
