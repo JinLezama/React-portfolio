4f3406b4fb9182c47bfaa6cfaae277ef465d4b8b
jin lezama <jingoto22@gmail.com>
2020-03-09 14:32:56 -0600

    Creating the Initial Blog Form Component

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 
+import BlogForm from "../blog/blog-form";
+
 ReactModal.setAppElement(".app-wrapper");
 
 export default class BlogModal extends Component {
@@ -29,7 +31,7 @@ export default class BlogModal extends Component {
            onRequestClose={() => {
                this.props.handleModalClose();
            }} isOpen={this.props.modalIsOpen}>
-               <h1>The modal</h1>
+              <BlogForm />
            </ReactModal>
         )
     }
