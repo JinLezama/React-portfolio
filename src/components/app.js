03cd9d2ae0d09e9889837562731d9d288f3bb054
jin lezama <jingoto22@gmail.com>
2020-03-09 22:54:24 -0600

    Building the Styles for the New Blog Icon with a Fixed Position on the Page

--- src/components/app.js
+++ src/components/app.js
@@ -7,8 +7,8 @@ import {
   faTrash,
   faSignOutAlt,
   faEdit,
-  faYinYang,
-  faRing
+  faSpinner,
+  faPlusCircle
 } from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -22,7 +22,7 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt, faEdit, faYinYang,faRing);
+library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
 
 export default class App extends Component {
   constructor(props) {
