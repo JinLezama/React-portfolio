82263191d69fd3d39bcf0602ec01abd59357047b
jin lezama <jingoto22@gmail.com>
2020-03-11 15:11:44 -0600

    Building About Page Styles for React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,33 @@
 import React from "react";
+import profilePicture from "../../../static/assets/images/bio/bioimage.jpg"
+import { convertFromHTML } from "draft-js";
 
 export default function() {
-  return <div>About</div>;
+  return (
+  <div className="content-page-wrapper">
+    <div 
+      className="left-column"
+      style={{
+        background: "url(" + profilePicture + ") no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    />
+    <div className="right-column">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      A ad quas labore harum nisi in perferendis nihil nobis eum est consequuntur,
+       debitis asperiores reprehenderit soluta reiciendis placeat tempora nostrum fuga?
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum nobis, optio 
+       possimus atque dolorem unde! Tempora nulla pariatur eligendi magnam, illum odit 
+       repellat expedita consequatur veniam aspernatur provident adipisci aut.
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa nulla tempora 
+       molestiae minima labore suscipit libero corrupti architecto? Nulla veniam quo, 
+       itaque voluptatum sit velit modi aliquam explicabo nihil error.
+       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor quo cupiditate, 
+       blanditiis voluptatem, magni voluptate harum eligendi nostrum repudiandae asperiores 
+       neque, nesciunt exercitationem fuga ipsa saepe deleniti dolore sed praesentium.
+       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti quasi qui vitae i
+    </div>
+  </div>
+  );
 }
