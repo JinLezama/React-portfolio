008563c21f0f2b230bddaebbc38ae79e6bddaf80
jin lezama <jingoto22@gmail.com>
2020-03-11 15:47:15 -0600

    Building Contact Page Styles for the React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,33 +1,36 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/bio/bioimage.jpg"
-import { convertFromHTML } from "draft-js";
+import profilePicture from "../../../static/assets/images/bio/bioimage.jpg";
 
 export default function() {
   return (
-  <div className="content-page-wrapper">
-    <div 
-      className="left-column"
-      style={{
-        background: "url(" + profilePicture + ") no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center"
-      }}
-    />
-    <div className="right-column">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      A ad quas labore harum nisi in perferendis nihil nobis eum est consequuntur,
-       debitis asperiores reprehenderit soluta reiciendis placeat tempora nostrum fuga?
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum nobis, optio 
-       possimus atque dolorem unde! Tempora nulla pariatur eligendi magnam, illum odit 
-       repellat expedita consequatur veniam aspernatur provident adipisci aut.
-       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa nulla tempora 
-       molestiae minima labore suscipit libero corrupti architecto? Nulla veniam quo, 
-       itaque voluptatum sit velit modi aliquam explicabo nihil error.
-       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor quo cupiditate, 
-       blanditiis voluptatem, magni voluptate harum eligendi nostrum repudiandae asperiores 
-       neque, nesciunt exercitationem fuga ipsa saepe deleniti dolore sed praesentium.
-       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti quasi qui vitae i
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis
+        dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis.
+        Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+        Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget
+        risus varius blandit sit amet non magna. Morbi leo risus, porta ac
+        consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida
+        at eget metus. Donec sed odio dui. Cras mattis consectetur purus sit
+        amet fermentum. Etiam porta sem malesuada magna mollis euismod. Nulla
+        vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque
+        ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non
+        commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
+        elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur
+        et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+        commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis
+        risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac
+        consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis
+        euismod semper.
+      </div>
     </div>
-  </div>
   );
 }
