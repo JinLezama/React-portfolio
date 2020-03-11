f25946c57f8e62dc5b3aec75eca3a24ee8a8596e
jin lezama <jingoto22@gmail.com>
2020-03-11 16:09:02 -0600

    Rendering the Portfolio Detail Data on the Screen

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,36 +1,53 @@
 import React, { Component } from "react";
 import axios from "axios";
-  
-  export default class PortfolioDetail extends Component {
-    constructor(props) {
-      super(props);
-    }
-  
-    componentWillMount() {
-      this.getPortfolioItem();
-    }
-  
-    getPortfolioItem() {
-      axios
-        .get(
-          `https://jinlezama.devcamp.space/portfolio/portfolio_items/${
-            this.props.match.params.slug
-          }`,
-          { withCredentials: true }
-        )
-        .then(response => {
-          console.log("res", response);
-        })
-        .catch(error => {
-          console.log("getportfolioitem error", error);
-        });
-    }
-  
-    render() {
-      return (
-        <div>
-          <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
-        </div>
-      );
-    }
+
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      portfolioItem: {}
+    };
   }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://jinlezama.devcamp.space/portfolio/portfolio_items/${
+          this.props.match.params.slug
+        }`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          portfolioItem: response.data.portfolio_item
+        });
+      })
+      .catch(error => {
+        console.log("getportfolioitem error", error);
+      });
+  }
+
+  render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url
+    } = this.state.portfolioItem;
+
+    return (
+      <div>
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </div>
+    );
+  }
+}
